f8527fc6421501cf52782df66467cf06bb06d4ab
progressify <a.porcelli90@gmail.com>
2020-08-05 16:31:25 +0200

    requirements updated class OpenApiService for creation of the sign and the generic call method, the keys are stored in config.ini file created a test case for test the various api endpoint
    TODO download method

--- api.py
+++ api.py
@@ -1,8 +1,33 @@
+import hashlib
 from configparser import ConfigParser
 
-config = ConfigParser()
-configpath = './'
-config.read(f'{configpath}config.ini')
+import requests
 
-print(config["SUNSKY"]["key"])
-print(config["SUNSKY"]["secret"])
+
+class OpenApiService:
+
+    def __init__(self, config_path='./'):
+        self.config = ConfigParser()
+        self.config.read(f'{config_path}config.ini')
+
+    def call(self, api_url, a_parameters):
+        a_parameters['signature'] = self.sign(a_parameters)
+        r = requests.post(api_url, data=a_parameters)
+        return r.text
+
+    def download(self, api_url, parameters, path):
+        # TODO
+        pass
+
+    def sign(self, a_parameters):
+        signature = ''
+        a_parameters['key'] = self.config["SUNSKY"]["key"]
+        for item in self.__ksort(a_parameters):
+            signature += item[1]
+
+        signature = f'{signature}@{self.config["SUNSKY"]["secret"]}'
+        m = hashlib.md5(signature.encode('utf-8'))
+        return m.hexdigest()
+
+    def __ksort(self, d):
+        return [(k, d[k]) for k in sorted(d.keys())]
---
+++ test_api_service.py
@@ -0,0 +1,64 @@
+import json
+import unittest
+
+from api import OpenApiService
+
+
+class TestApi(unittest.TestCase):
+
+    def setUp(self):
+        self.oas = OpenApiService()
+
+    def test_download(self):
+        # TODO
+        pass
+
+    def test_get_categories(self):
+        """
+        Fetch the categories ever changed since gmtModifiedStart
+        """
+        url_categories = "http://www.sunsky-api.com/openapi/category!getChildren.do"
+        parameters = {'gmtModifiedStart': '10/31/2013'}
+        result = self.oas.call(url_categories, parameters)
+        self.assertEqual(json.loads(result)['result'], 'success')
+
+    def test_get_products(self):
+        """
+        Fetch the products ever changed since gmtModifiedStart
+        """
+        url_products = "http://www.sunsky-api.com/openapi/product!search.do"
+        parameters = {'gmtModifiedStart': '10/31/2012'}
+        result = self.oas.call(url_products, parameters)
+        self.assertEqual(json.loads(result)['result'], 'success')
+
+    def test_get_product_details(self):
+        """
+        Fetch the details for the product
+        """
+        url_product_detail = "http://www.sunsky-api.com/openapi/product!detail.do"
+        parameters = {'itemNo': 'SAS8251B', 'withLogo': 'true'}
+        result = self.oas.call(url_product_detail, parameters)
+        self.assertEqual(json.loads(result)['result'], 'success')
+
+    def test_get_countries_and_states(self):
+        """
+        Fetch the countries and states
+        """
+        url_product_detail = "http://www.sunsky-api.com/openapi/order!getCountries.do"
+        result = self.oas.call(url_product_detail, {})
+        self.assertEqual(json.loads(result)['result'], 'success')
+
+    def test_calculate_price(self):
+        """
+        Calculate the prices and freights for the items
+        """
+        url_product_detail = "http://www.sunsky-api.com/openapi/order!getPricesAndFreights.do"
+        parameters = {
+            'countryId': '41',
+            'items.1.itemNo': 'SAS8251B',
+            'items.1.qty': '20',
+            'items.2.itemNo': 'SAS8669W',
+            'items.2.qty': '5'
+        }
+        result = self.oas.call(url_product_detail, parameters)
+        self.assertEqual(json.loads(result)['result'], 'success')
